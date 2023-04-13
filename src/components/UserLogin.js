caad143f72b88751818ac923bb73021983de608c
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-04-13 00:27:40 -0400

    Created UserLogin component. Created connection to api login route. Created handler to set token on localStorage.

---
+++ src/components/UserLogin.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import "./UserSignupAndLogin.css";
+import axios from "axios";
+
+const TOKEN_KEY = process.env.REACT_APP_TOKEN_HEADER_KEY;
+
+const UserSignup = () => {
+  const valuesReset = {
+    email: "",
+    password: ""
+  };
+
+  const [user, setUser] = useState(valuesReset);
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState(valuesReset);
+
+  if (error) console.log(error);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+    setMessage(valuesReset);
+    setError("");
+  };
+
+  const postUser = async () => {
+    const { data } = await axios.post("http://localhost:8000/users/login", {
+      email: user.email,
+      password: user.password,
+    });
+
+    if ("error" in data) {
+      setError(data.error);
+    } else {
+      localStorage.setItem(TOKEN_KEY, data.token);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    postUser();
+    setUser(valuesReset);
+  };
+
+  return (
+    <div className="sign-up-card">
+      <form
+        className="sign-up-form"
+        onSubmit={(e) => handleSubmit(e)}
+        autoComplete="off"
+      >
+        <div className="title">TICKET MANAGER</div>
+        <p className="sign-up-title">Login</p>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={user.email}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">
+          Password
+          <span className="password-message">{message.password}</span>
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={user.password}
+          onChange={handleChange}
+        />
+        <div className="error-message">{error}</div>
+        <button className="submit-button" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default UserSignup;
