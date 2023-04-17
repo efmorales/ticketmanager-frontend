1e49b9a938a799335978f0739a45f5e704af4cce
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-04-17 15:07:27 -0400

    Created basic/placeholder nav bar.

--- src/layouts/AppLayout.js
+++ src/layouts/AppLayout.js
@@ -1,4 +1,5 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
+import { FaRegUserCircle } from "react-icons/fa";
 
 import "./AppLayout.css";
 
@@ -6,9 +7,10 @@ const AppLayout = () => {
   return (
     <>
       <header className="app-header">
-        <h1 className="header-title">
-          
-        </h1>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/signup">Signup</NavLink>
+        <FaRegUserCircle />
       </header>
       <main>
         <Outlet />
