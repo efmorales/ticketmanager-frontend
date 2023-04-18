d401fd028f41399163d1ce2e6231d9d091b24ee7
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-04-18 12:16:50 -0400

    Added /profile route/component.

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ import AppLayout from "../layouts/AppLayout";
 import Home from "./Home";
 import UserSignup from "./UserSignup";
 import UserLogin from "./UserLogin";
+import UserProfile from "./UserProfile";
 import Error from "./Error";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="login" element={<UserLogin />} />
         <Route path="signup" element={<UserSignup />} />
+        <Route path="profile" element={<UserProfile />} />
         <Route path="*" element={<Error />} />
       </Route>
     )
