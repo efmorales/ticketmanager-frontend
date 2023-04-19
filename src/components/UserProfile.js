1ceb4bd5e329e7a2104bc22331c9febfdc536d69
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-04-19 14:28:14 -0400

    Rendered user info from global AuthContext, useing useAuth.

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,7 +1,14 @@
+import { useAuth } from "../auth/AuthContext";
+
 const UserProfile = () => {
+  const { loggedInUser } = useAuth();
+
   return (
-    <h1>This is my User Profile!</h1>
+    <>
+      <h1>{loggedInUser.name}</h1>
+      <h1>{loggedInUser.email}</h1>
+    </>
   );
-}
- 
+};
+
 export default UserProfile;
