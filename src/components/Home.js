d97bfb4c3640c73de1ec4f61e360a68675d7824d
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-04-19 14:22:40 -0400

    Imported useAuth and andded conditional render of user's name to home greeting.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
+import { useAuth } from "../auth/AuthContext";
+
 const Home = () => {
-    return (
-        <h1>This is my Home!</h1>
-    );
-}
- 
+  const { loggedInUser } = useAuth();
+
+  return (
+    <h1>
+      Hello{loggedInUser && ` ${loggedInUser.name}`}, welcome to the Ticket
+      Manager Home Page!
+    </h1>
+  );
+};
+
 export default Home;
