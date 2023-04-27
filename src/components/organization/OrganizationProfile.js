2c110d7e318ec79cf4123cb65ca51c011bad7919
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-04-27 17:43:21 -0400

    Created placeholder component.

---
+++ src/components/organization/OrganizationProfile.js
@@ -0,0 +1,13 @@
+const OrganizationProfile = () => {
+  const styles = {
+    padding: "1em 4em",
+  };
+  return (
+    <div className="" style={styles}>
+      <h1>This is the Organization's Profile Page!</h1>
+      <p>The main dashboad for the Organization can go here.</p>
+    </div>
+  );
+};
+
+export default OrganizationProfile;
