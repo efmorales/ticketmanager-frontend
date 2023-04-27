1b343fd9bf6d325a7b7433ab62470f38db13ba4b
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-04-27 12:26:36 -0400

    Added organization profile link to top of side-nav.

--- src/layouts/OrganizationLayout.js
+++ src/layouts/OrganizationLayout.js
@@ -1,16 +1,33 @@
-import { Outlet } from "react-router-dom";
-import "./OrganizationLayout.css"
+import { Outlet, NavLink } from "react-router-dom";
+import { FaRegUserCircle } from "react-icons/fa";
+
+import "./OrganizationLayout.css";
 
 const OrganizationLayout = () => {
+
+  const handleClick = (e) => {
+    console.log(`${e.target.innerText} clicked!`);
+  };
+
   return (
     <div className="organization-page">
       <nav className="organization-nav">
+        <div className="profile-link" onClick={handleClick}>
+          <NavLink to="/organization/profile">
+            <FaRegUserCircle size={40} />
+            <div>
+              Organization Name
+              <br />
+              <span className="display-type">Organization</span>
+            </div>
+          </NavLink>
+        </div>
         <ul>
-          <li>Link 1</li>
-          <li>Link 2</li>
-          <li>Link 3</li>
-          <li>Link 4</li>
-          <li>Link 5</li>
+          <li onClick={handleClick}>Projects</li>
+          <li onClick={handleClick}>Members</li>
+          <li onClick={handleClick}>Link 3</li>
+          <li onClick={handleClick}>Link 4</li>
+          <li onClick={handleClick}>Link 5</li>
         </ul>
       </nav>
       <section className="organization-content">
