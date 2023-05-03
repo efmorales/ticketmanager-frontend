611be5fcb4d92b815adf48056eaf7a0338aa6a10
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-05-03 18:23:10 -0400

    Added code to display list of organization members.

--- src/components/organization/OrganizationMembers.js
+++ src/components/organization/OrganizationMembers.js
@@ -1,18 +1,31 @@
+import { Link } from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
+import { FaRegUserCircle } from "react-icons/fa";
+
+import "./OrganizationMembers.css";
+
 const OrganizationMembers = () => {
-  const styles = {
-    padding: "1em 4em",
-  };
+  const { organization, members } = useOutletContext();
+
   return (
-    <div className="members-page" style={styles}>
-      <h1>These are our members!</h1>
-      <p>List of members will be displayed here</p> <br />
-      <p>To-do functionalities: </p>
-      <ul>
-        <li>Find Member</li>
-        <li>Add Member</li>
-        <li>Remove Member</li>
-        <li>Change Member Permissions</li>
-      </ul>
+    <div className="members-page">
+      <div className="member-list-controls">
+        <div>Controls go here</div>
+      </div>
+      <div className="add-new-member">+ Add New Member</div>
+      {members.map((member) => {
+        return (
+          <Link to={`${member._id}`}>
+            <div className="member-list-card" key={member._id}>
+              <FaRegUserCircle size={40} />
+              <div className="member-list-card-info">
+                <h3>{member.user.name}</h3>
+                <p>{member.permissions}</p>
+              </div>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 };
