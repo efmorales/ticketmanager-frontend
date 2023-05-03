9e0cb649124e703ab39d68c5f15256e18d20123b
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-05-03 18:25:49 -0400

    Created copy of 'Projects' component for use with Organizations.

--- src/components/organization/OrganizationProjects.js
+++ src/components/organization/OrganizationProjects.js
@@ -1,14 +1,53 @@
+import React, { useEffect, useState } from "react";
+import api from "../../auth/api";
+import { Link, useOutletContext } from "react-router-dom";
+import { FaPlusCircle } from "react-icons/fa";
+import "../Projects.css";
+
+const API_BASE_URL = process.env.REACT_APP_API_URL;
+const headerToken = process.env.REACT_APP_TOKEN_HEADER_KEY;
+
 const OrganizationProjects = () => {
-  const styles = {
-    padding: "1em 4em",
-  };
+  const { organization, members } = useOutletContext();
+
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const token = localStorage.getItem(headerToken);
+        if (token) {
+          const response = await api.get(
+            `${API_BASE_URL}/organizations/${organization._id}/projects`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          setProjects(response.data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
   return (
-    <div className="members-page" style={styles}>
-      <h1>Organization Projects!</h1>
-      <p>List of organization projects will be displayed here</p> <br />
-      <p>To-do functionalities: </p>
+    <div className="projects-container">
+      <Link to="new" className="new-project">
+        <h3>ADD NEW PROJECT</h3>
+        <FaPlusCircle size={30} className="circle-icon" />
+      </Link>
+      <h1>Projects</h1>
       <ul>
-        <li>Import Projects component and modify for Org use as needed</li>
+        {projects.map((project) => (
+          <li key={project._id}>
+            <Link to={project._id}>{project.name}</Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
