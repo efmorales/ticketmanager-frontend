458a5d1964d11286954c58c8b2e7898e7e7ea91b
efmorales <54375116+efmorales@users.noreply.github.com>
2023-05-04 12:28:49 -0400

    initial changes to UserDashboard component

--- src/components/user/UserDashboard.js
+++ src/components/user/UserDashboard.js
@@ -1,13 +1,68 @@
+import { useState, useEffect } from "react";
+// import { useAuth } from "../auth/AuthContext";
+// import api from "../auth/api";
+import { Link } from "react-router-dom";
+const TOKEN_KEY = process.env.REACT_APP_TOKEN_HEADER_KEY;
+
 const UserDashboard = () => {
-  const styles = {
-    padding: "1em 4em",
-  };
+  // const { loggedInUser } = useAuth();
+  // const [assignedTickets, setAssignedTickets] = useState([]);
+
+  //   useEffect(() => {
+  //   const fetchAssignedTickets = async () => {
+  //     try {
+  //       // Fetch user's projects
+  //       const { data: projectsData } = await api.get("/projects", {
+  //         headers: {
+  //           Authorization: `Bearer ${localStorage.getItem(TOKEN_KEY)}`,
+  //         },
+  //       });
+
+  //       // Fetch tickets for each project and filter by assigned user
+  //       const assignedTicketsPromises = projectsData.map(async (project) => {
+  //         const { data: ticketsData } = await api.get(`/projects/${project._id}`, {
+  //           headers: {
+  //             Authorization: `Bearer ${localStorage.getItem(TOKEN_KEY)}`,
+  //           },
+  //         });
+  //         console.log(ticketsData)
+  //         return ticketsData.tickets.filter(
+  //           (ticket) => ticket.assignedTo[0] && ticket.assignedTo[0]._id === projectsData.user._id
+  //         );
+  //       });
+
+  //       const assignedTicketsArrays = await Promise.all(assignedTicketsPromises);
+  //       const flattenedAssignedTickets = assignedTicketsArrays.flat();
+  //       setAssignedTickets(flattenedAssignedTickets);
+  //     } catch (error) {
+  //       console.error("Failed to fetch assigned tickets:", error);
+  //     }
+  //   };
+
+  //   fetchAssignedTickets();
+  // }, []);
+
   return (
-    <div className="" style={styles}>
-      <h1>This is the User Dashboard!</h1>
-      <p>User Dashboard can be displayed here.</p>
-    </div>
-  );
+        <div className="home-container">
+          {/* <h1>
+            Hello{loggedInUser && ` ${loggedInUser?.name}`}, welcome to the Ticket
+            Manager Home Page!
+          </h1> */}
+    
+          <div>
+            <h1>Your Assigned Tickets</h1>
+            {/* <ul>
+              {assignedTickets.map((ticket) => (
+                <li key={ticket._id}>
+                  <Link to={`/user/projects/${ticket.projectId}/tickets/${ticket._id}`}>
+                    {ticket.title}
+                  </Link>
+                </li>
+              ))}
+            </ul> */}
+          </div>
+        </div>
+      );
 }
  
 export default UserDashboard;
