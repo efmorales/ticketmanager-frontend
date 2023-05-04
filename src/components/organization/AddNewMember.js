62e26d552af706e34d003f80031fb943fa9f6b46
Michael Fernandez <michael.fernandez@codeimmersives.com>
2023-05-04 01:15:45 -0400

    Moved members list to separate component. Created placeholder component for 'add new member' form.

---
+++ src/components/organization/AddNewMember.js
@@ -0,0 +1,14 @@
+import { FaArrowCircleLeft } from "react-icons/fa";
+
+const AddNewMember = ({ message }) => {
+  return (
+    <>
+      <h1>
+        Add New Member Page will go here!!
+      </h1>
+      {message}
+    </>
+  );
+};
+
+export default AddNewMember;
--- src/components/organization/OrganizationMembers.js
+++ src/components/organization/OrganizationMembers.js
@@ -1,31 +1,49 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+// import { Link } from "react-router-dom";
 import { useOutletContext } from "react-router-dom";
-import { FaRegUserCircle } from "react-icons/fa";
+import { FaPlusCircle, FaArrowCircleLeft, FaSearch } from "react-icons/fa";
+
+import MembersList from "./MembersList";
+import AddNewMember from "./AddNewMember";
 
 import "./OrganizationMembers.css";
 
-const OrganizationMembers = () => {
+const OrganizationMembers = ({ children }) => {
+  const [isListSelected, setIsListSelected] = useState(true);
+
   const { organization, members } = useOutletContext();
+
+  const handleToggle = () => {
+    setIsListSelected(!isListSelected);
+  };
 
   return (
     <div className="members-page">
       <div className="member-list-controls">
-        <div>Controls go here</div>
-      </div>
-      <div className="add-new-member">+ Add New Member</div>
-      {members.map((member) => {
-        return (
-          <Link to={`${member._id}`}>
-            <div className="member-list-card" key={member._id}>
-              <FaRegUserCircle size={40} />
-              <div className="member-list-card-info">
-                <h3>{member.user.name}</h3>
-                <p>{member.permissions}</p>
-              </div>
+        <div onClick={handleToggle}>
+          {isListSelected ? (
+            <div className="member-control">
+              <FaPlusCircle size={20} /> <span>Add New Member</span>
             </div>
-          </Link>
-        );
-      })}
+          ) : (
+            <div className="member-control">
+              <FaArrowCircleLeft size={20} /> <span>Back to Members</span>
+            </div>
+          )}
+        </div>
+      </div>
+      {/* <div className="member-control search-members">
+        <FaSearch size={20} /> <span>Search Members</span>
+      </div> */}
+      {/* <Link to="new" className="add-new-member">
+        
+        <span>ADD NEW MEMBER</span>
+      </Link> */}
+      {isListSelected ? (
+        <MembersList members={members} />
+      ) : (
+        <AddNewMember message="Hello Props!" />
+      )}
     </div>
   );
 };
