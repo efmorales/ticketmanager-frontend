528ac65180ee046bf023747251dea2fa135400c8
efmorales <54375116+efmorales@users.noreply.github.com>
2023-05-04 22:51:25 -0400

    added UserDashboard component and styling

--- src/components/user/UserDashboard.js
+++ src/components/user/UserDashboard.js
@@ -1,68 +1,67 @@
-import { useState, useEffect } from "react";
-// import { useAuth } from "../auth/AuthContext";
-// import api from "../auth/api";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import api from "../../auth/api";
+import "./UserDashboard.css";
+
 const TOKEN_KEY = process.env.REACT_APP_TOKEN_HEADER_KEY;
 
 const UserDashboard = () => {
-  // const { loggedInUser } = useAuth();
-  // const [assignedTickets, setAssignedTickets] = useState([]);
+  const [assignedTickets, setAssignedTickets] = useState([]);
+  const [latestProjects, setLatestProjects] = useState([]);
 
-  //   useEffect(() => {
-  //   const fetchAssignedTickets = async () => {
-  //     try {
-  //       // Fetch user's projects
-  //       const { data: projectsData } = await api.get("/projects", {
-  //         headers: {
-  //           Authorization: `Bearer ${localStorage.getItem(TOKEN_KEY)}`,
-  //         },
-  //       });
+  useEffect(() => {
+    const fetchAssignedTickets = async () => {
+      try {
+        const response = await api.get("/tickets/assigned", {
+          headers: { Authorization: `Bearer ${localStorage.getItem(TOKEN_KEY)}` },
+        });
 
-  //       // Fetch tickets for each project and filter by assigned user
-  //       const assignedTicketsPromises = projectsData.map(async (project) => {
-  //         const { data: ticketsData } = await api.get(`/projects/${project._id}`, {
-  //           headers: {
-  //             Authorization: `Bearer ${localStorage.getItem(TOKEN_KEY)}`,
-  //           },
-  //         });
-  //         console.log(ticketsData)
-  //         return ticketsData.tickets.filter(
-  //           (ticket) => ticket.assignedTo[0] && ticket.assignedTo[0]._id === projectsData.user._id
-  //         );
-  //       });
+        setAssignedTickets(response.data.tickets);
+      } catch (error) {
+        console.error("Failed to fetch assigned tickets:", error);
+      }
+    };
 
-  //       const assignedTicketsArrays = await Promise.all(assignedTicketsPromises);
-  //       const flattenedAssignedTickets = assignedTicketsArrays.flat();
-  //       setAssignedTickets(flattenedAssignedTickets);
-  //     } catch (error) {
-  //       console.error("Failed to fetch assigned tickets:", error);
-  //     }
-  //   };
+    const fetchLatestProjects = async () => {
+      try {
+        const response = await api.get("/projects/latest", {
+          headers: { Authorization: `Bearer ${localStorage.getItem(TOKEN_KEY)}` },
+        });
 
-  //   fetchAssignedTickets();
-  // }, []);
+        setLatestProjects(response.data.projects);
+      } catch (error) {
+        console.error("Failed to fetch latest projects:", error);
+      }
+    };
+
+    fetchAssignedTickets();
+    fetchLatestProjects();
+  }, []);
 
   return (
-        <div className="home-container">
-          {/* <h1>
-            Hello{loggedInUser && ` ${loggedInUser?.name}`}, welcome to the Ticket
-            Manager Home Page!
-          </h1> */}
-    
-          <div>
-            <h1>Your Assigned Tickets</h1>
-            {/* <ul>
-              {assignedTickets.map((ticket) => (
-                <li key={ticket._id}>
-                  <Link to={`/user/projects/${ticket.projectId}/tickets/${ticket._id}`}>
-                    {ticket.title}
-                  </Link>
-                </li>
-              ))}
-            </ul> */}
-          </div>
-        </div>
-      );
-}
- 
+    <div className="user-dashboard-container">
+      <h1> User Dashboard </h1>
+      <h2>Assigned Tickets</h2>
+      <ul>
+
+        {assignedTickets.filter(ticket => ticket.projectId).map((ticket) => (
+          <li key={ticket._id}>
+            {ticket.title} ({ticket.projectId.name})
+          </li>
+        ))}
+      </ul>
+      <h2>Latest Projects</h2>
+      <ul>
+        {latestProjects.map((project) => (
+          <li key={project._id}>
+            <Link to={`/user/projects/${project._id}`}>
+            {project.name} - Last Updated: {new Date(project.updatedAt).toLocaleString()}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
 export default UserDashboard;
